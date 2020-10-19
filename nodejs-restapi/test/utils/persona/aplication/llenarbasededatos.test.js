0f3bdc8416f8ff6f4e3ac5b74d95bb946e78b91c
BrandoSaenzRojas <brandosaenzrojas@hotmail.com>
2020-10-19 11:47:45 -0500

    delete llenarbase

--- nodejs-restapi/test/utils/persona/aplication/llenarbasededatos.test.js
+++
@@ -1,8 +0,0 @@
-const llenar = require('../../../../src/persona/aplication/llenarBaseDeDatos');
-
- 
-it('Test Fn(getOperation)', async() => {
-    // const event = {};
-    // const data = await llenar(event);
-    // expect(data);
-});
